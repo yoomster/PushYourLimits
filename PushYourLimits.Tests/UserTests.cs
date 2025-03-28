43cb9a9ed2d0482815b362fd51a69789cce9e7dc
yoomster <nperenboom@hotmail.com>
2025-03-28 17:10:14 +0100

    unit test proj set-up for testing user units

---
+++ PushYourLimits.Tests/UserTests.cs
@@ -0,0 +1,14 @@
+﻿using PushYourLimits.Application.Services;
+
+namespace PushYourLimits.Tests
+{
+    public class UserTests
+    {
+        private readonly UserService _sut;
+
+        public UserTests()
+        {
+            _sut = new UserService();
+        }
+    }
+}
