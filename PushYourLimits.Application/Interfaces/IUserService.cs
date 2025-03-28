5e553a3e08a07eb2de592a6142b14fc6dc1bc90b
yoomster <nperenboom@hotmail.com>
2025-03-28 17:05:00 +0100

    Added User services + interface

---
+++ PushYourLimits.Application/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using PushYourLimits.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace PushYourLimits.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task CreateUserAccount(User user);
+
+        Task DeleteUserAccount(int userId);
+
+    }
+}
---
+++ PushYourLimits.Application/Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using PushYourLimits.Application.Interfaces;
+using PushYourLimits.Domain;
+
+namespace PushYourLimits.Application.Services
+{
+    public class UserService : IUserService
+    {
+        public Task CreateUserAccount(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserAccount(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
