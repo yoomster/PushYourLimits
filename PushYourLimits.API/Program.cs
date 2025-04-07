bdf0ad4735163384998255f0e36c15e86c3115dd
yoomster <nperenboom@hotmail.com>
2025-04-07 23:52:15 +0200

    added user api

--- PushYourLimits.API/Program.cs
+++ PushYourLimits.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using PushYourLimits.Application.Interfaces;
 using PushYourLimits.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,7 @@ builder.Services.AddDbContext<PYLDbContext>(optionsBuilder =>
 {
     optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("LocalDbConnectionString"));
 });
+//builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
